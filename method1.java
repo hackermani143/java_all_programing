ffa12cda2244c01783dd29a0ba1094a76a90ae58
hackermani143 <120126686+hackermani143@users.noreply.github.com>
2024-08-13 23:23:31 +0530

    Add files via upload

---
+++ method1.java
@@ -0,0 +1,14 @@
+
+// create a java apllication where we have one class it contains non_static method m1  havaing no parameters ,returns nothing we we invoke these method under main method it have to display any user frindly message 
+class secondday{
+  //  int b=20;
+    void m1( ){
+      System.out.println("hacker");
+    }
+    public static void main(String[] args) {
+        secondday a=new secondday();
+        a.m1( );
+        // System.out.println(a.b);
+
+    }
+}
